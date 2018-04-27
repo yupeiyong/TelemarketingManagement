819cb752daac0b5e8e0478f0e3e18c9bcb232d82
yupeiyong <2758318132@qq.com>
2018-04-27 16:03:36 +0800

    create project

---
+++ TelemarketingManagement/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TelemarketingManagement
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TelemarketingManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TelemarketingManagement.MvcApplication" Language="C#" %>
