fa8354e7c2829b7115a907805c67ed797cf14645
yupeiyong <2758318132@qq.com>
2018-04-28 22:18:59 +0800

    add common files

---
+++ Common/Configs/DBConnection.cs
@@ -0,0 +1,29 @@
+﻿
+using Common.Security;
+
+namespace Common.Configs
+{
+    public class DbConnection
+    {
+        public static bool Encrypt { get; set; }
+        public DbConnection(bool encrypt)
+        {
+            Encrypt = encrypt;
+        }
+        public static string ConnectionString
+        {
+            get
+            {
+                string connection = System.Configuration.ConfigurationManager.ConnectionStrings["NiceDbContext"].ConnectionString;
+                if (Encrypt == true)
+                {
+                    return DesEncrypt.Decrypt(connection);
+                }
+                else
+                {
+                    return connection;
+                }
+            }
+        }
+    }
+}
---
+++ Common/Operator/OnlineUserProvider.cs
@@ -0,0 +1,56 @@
+﻿
+
+using Common.Json;
+using Common.Security;
+using Common.Web;
+using System;
+
+namespace Common.Operator
+{
+    public class OnlineUserProvider
+    {
+        public static OnlineUserProvider Provider
+        {
+            get { return new OnlineUserProvider(); }
+        }
+        private string OnlineUserKey = $"Online_User_{DateTime.Now.Date.ToString("yyyy-MM-dd")}";
+        private string LoginProvider = Common.Configs.Configs.GetValue("LoginProvider");
+
+        public OperatorModel GetCurrent()
+        {
+            OperatorModel operatorModel = new OperatorModel();
+            if (LoginProvider == "Cookie")
+            {
+                operatorModel = DesEncrypt.Decrypt(WebHelper.GetCookie(OnlineUserKey).ToString()).ToObject<OperatorModel>();
+            }
+            else
+            {
+                operatorModel = DesEncrypt.Decrypt(WebHelper.GetSession(OnlineUserKey).ToString()).ToObject<OperatorModel>();
+            }
+            return operatorModel;
+        }
+        public void AddCurrent(OperatorModel operatorModel)
+        {
+            if (LoginProvider == "Cookie")
+            {
+                WebHelper.WriteCookie(OnlineUserKey, DesEncrypt.Encrypt(operatorModel.ToJson()), 60);
+            }
+            else
+            {
+                WebHelper.WriteSession(OnlineUserKey, DesEncrypt.Encrypt(operatorModel.ToJson()));
+            }
+            WebHelper.WriteCookie("Online_User_mac", Md5.md5(Net.Net.GetMacByNetworkInterface().ToJson(), 32));
+        }
+        public void RemoveCurrent()
+        {
+            if (LoginProvider == "Cookie")
+            {
+                WebHelper.RemoveCookie(OnlineUserKey.Trim());
+            }
+            else
+            {
+                WebHelper.RemoveSession(OnlineUserKey.Trim());
+            }
+        }
+    }
+}
