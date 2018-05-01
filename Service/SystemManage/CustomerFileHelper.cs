4e25c65fdac87d7c2cd8936bcb3281c9e002e869
yupeiyong <2758318132@qq.com>
2018-05-02 00:03:56 +0800

    modify

--- Service/SystemManage/CustomerFileHelper.cs
+++ Service/SystemManage/CustomerFileHelper.cs
@@ -15,7 +15,6 @@ namespace Service.SystemManage
     {
         public static int MaxFileSize { get; set; } = 1024 * 500;
 
-
         /// <summary>
         /// 从文件导入资料到数据库,必须和模板上的列一致
         /// </summary>
@@ -121,105 +120,8 @@ namespace Service.SystemManage
                     }
                 }
             }
-            return new ImportedResultDto { Count = 1, Title = "" };
+            return new ImportedResultDto { Count = 1, Title = "" }; 
         }
-
-
-
-
-        /*
-         
-             if (workbook != null)  
-                    {  
-                        sheet = workbook.GetSheetAt(0);//读取第一个sheet，当然也可以循环读取每个sheet  
-                        dataTable = new DataTable();  
-                        if (sheet != null)  
-                        {  
-                            int rowCount = sheet.LastRowNum;//总行数  
-                            if (rowCount > 0)  
-                            {  
-                                IRow firstRow = sheet.GetRow(0);//第一行  
-                                int cellCount = firstRow.LastCellNum;//列数  
-  
-                                //构建datatable的列  
-                                if (isColumnName)  
-                                {  
-                                    startRow = 1;//如果第一行是列名，则从第二行开始读取  
-                                    for (int i = firstRow.FirstCellNum; i < cellCount; ++i)  
-                                    {  
-                                        cell = firstRow.GetCell(i);  
-                                        if (cell != null)  
-                                        {  
-                                            if (cell.StringCellValue != null)  
-                                            {  
-                                                column = new DataColumn(cell.StringCellValue);  
-                                                dataTable.Columns.Add(column);  
-                                            }  
-                                        }  
-                                    }  
-                                }  
-                                else  
-                                {  
-                                    for (int i = firstRow.FirstCellNum; i < cellCount; ++i)  
-                                    {  
-                                        column = new DataColumn("column" + (i + 1));  
-                                        dataTable.Columns.Add(column);  
-                                    }  
-                                }  
-  
-                                //填充行  
-                                for (int i = startRow; i <= rowCount; ++i)  
-                                {  
-                                    row = sheet.GetRow(i);  
-                                    if (row == null) continue;  
-  
-                                    dataRow = dataTable.NewRow();  
-                                    for (int j = row.FirstCellNum; j < cellCount; ++j)  
-                                    {  
-                                        cell = row.GetCell(j);                                          
-                                        if (cell == null)  
-                                        {  
-                                            dataRow[j] = "";  
-                                        }  
-                                        else  
-                                        {  
-                                            //CellType(Unknown = -1,Numeric = 0,String = 1,Formula = 2,Blank = 3,Boolean = 4,Error = 5,)  
-                                            switch (cell.CellType)  
-                                            {  
-                                                case CellType.Blank:  
-                                                    dataRow[j] = "";  
-                                                    break;  
-                                                case CellType.Numeric:  
-                                                    short format = cell.CellStyle.DataFormat;  
-                                                    //对时间格式（2015.12.5、2015/12/5、2015-12-5等）的处理  
-                                                    if (format == 14 || format == 31 || format == 57 || format == 58)  
-                                                        dataRow[j] = cell.DateCellValue;  
-                                                    else  
-                                                        dataRow[j] = cell.NumericCellValue;  
-                                                    break;  
-                                                case CellType.String:  
-                                                    dataRow[j] = cell.StringCellValue;  
-                                                    break;  
-                                            }  
-                                        }  
-                                    }  
-                                    dataTable.Rows.Add(dataRow);  
-                                }  
-                            }  
-                        }  
-                    }  
-                }  
-                return dataTable;  
-            }  
-            catch (Exception)  
-            {  
-                if (fs != null)  
-                {  
-                    fs.Close();  
-                }  
-                return null;  
-            }  
-             */
 
         /// <summary>
         /// 上传文件
